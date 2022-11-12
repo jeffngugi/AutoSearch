0c4f4c1e6b3b33d3aeee43a6a253a72f2f12e253
jeffngugi <ngugimwathi254@gmail.com>
2022-11-12 18:01:50 +0300

    valid url app checker

---
+++ App.tsx
@@ -0,0 +1,55 @@
+import {StyleSheet, TextInput, SafeAreaView} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import WebView from 'react-native-webview';
+
+const App = () => {
+  const [searchText, setSearchText] = useState<string>('');
+  // if(searchText)
+  function validURL(str) {
+    var pattern = new RegExp(
+      '^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$',
+      'i',
+    ); // fragment locator
+    return !!pattern.test(str);
+  }
+
+  // useEffect(() => {
+  //   validURL(searchText);
+  // }, [searchText]);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.txtInput}
+        onChangeText={text => setSearchText(text)}
+      />
+      {validURL(searchText) ? (
+        <WebView source={{uri: searchText}} containerStyle={styles.webView} />
+      ) : null}
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  txtInput: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 8,
+    borderColor: 'gray',
+  },
+  webView: {
+    flex: 1,
+  },
+});
